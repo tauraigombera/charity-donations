f4cad053fe1f17a697638d80cc700aac540c1cc4
TauG <tauraigombera@gmail.com>
2023-07-29 12:15:15 +0200

    added contact Model in the Entities

---
+++ CharityDonations.Api/Entities/Contact.cs
@@ -0,0 +1,11 @@
+﻿namespace CharityDonations.Api.Entities;
+
+public class Contact
+{
+    public int Id { get; set;}
+    public required string Email { get; set;}
+    public required string PhoneNumber { get; set; }
+    public required string Address1 { get; set; }
+    public required string Address2 { get; set; }
+    public string? Address3 { get; set; }
+}
