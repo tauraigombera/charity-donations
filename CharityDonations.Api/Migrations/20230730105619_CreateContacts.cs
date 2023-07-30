0cfa31d4866f1be25f56bcecf57cf57bc62d4e53
TauG <tauraigombera@gmail.com>
2023-07-30 14:31:23 +0200

    added CreateContact migration

---
+++ CharityDonations.Api/Migrations/20230730105619_CreateContacts.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CharityDonations.Api.Migrations
+{
+    /// <inheritdoc />
+    public partial class CreateContacts : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Contacts",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Email = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    PhoneNumber = table.Column<string>(type: "nvarchar(20)", maxLength: 20, nullable: false),
+                    Address1 = table.Column<string>(type: "nvarchar(150)", maxLength: 150, nullable: false),
+                    Address2 = table.Column<string>(type: "nvarchar(150)", maxLength: 150, nullable: true),
+                    Address3 = table.Column<string>(type: "nvarchar(150)", maxLength: 150, nullable: true),
+                    OrganizationId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Contacts", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Contacts_Organizations_OrganizationId",
+                        column: x => x.OrganizationId,
+                        principalTable: "Organizations",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Contacts_OrganizationId",
+                table: "Contacts",
+                column: "OrganizationId",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Contacts");
+        }
+    }
+}
