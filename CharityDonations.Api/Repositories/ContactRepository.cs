88edc513d442ad7540ce6ade45e9eeb652a40a21
TauG <tauraigombera@gmail.com>
2023-07-30 04:55:18 +0200

    added ContactRepository

---
+++ CharityDonations.Api/Repositories/ContactRepository.cs
@@ -0,0 +1,43 @@
+﻿using CharityDonations.Api.Data;
+using CharityDonations.Api.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace CharityDonations.Api.Repositories;
+
+public class ContactRepository : IContactRepository
+{
+    private readonly CharityOrganizationsContext dbContext;
+
+    public ContactRepository(CharityOrganizationsContext dbContext)
+    {
+        this.dbContext = dbContext;
+    }
+    public async Task<Contact?> GetAsync(int contactId)
+    {
+         return await dbContext.Contacts.FindAsync(contactId);
+    }
+    public async Task<Contact?> GetByOrganizationIdAsync(int organizationId)
+    {
+        return await dbContext.Contacts
+            .Where(c => c.OrganizationId == organizationId)
+            .FirstOrDefaultAsync();
+    }  
+    public async Task CreateAsync(Contact contact)
+    {
+        dbContext.Contacts.Add(contact);
+        await dbContext.SaveChangesAsync();
+    }
+
+    public async Task UpdateAsync(Contact updatedContact)
+    {
+        dbContext.Update(updatedContact);
+        await dbContext.SaveChangesAsync();
+    }
+
+    public async Task DeleteAsync(int contactId)
+    {
+        await dbContext.Contacts.Where(organization => organization.Id == contactId)
+                                .ExecuteDeleteAsync();
+    }
+
+}
