782419706da202e2a3810b1b5092d75821ab4fdb
TauG <tauraigombera@gmail.com>
2023-08-03 11:18:27 +0200

    added BankAccount model

---
+++ CharityDonations.Api/Models/BankAccount.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CharityDonations.Api.Models;
+
+public class BankAccount
+{
+    public int Id { get; set; }
+    [Required]
+    [StringLength(20)]
+    public required string AccountNumber { get; set; }
+    [Required]
+    [StringLength(50)]
+    public required string AccountHolderName { get; set; }
+    [Required]
+    [StringLength(50)]
+    public required string BankName { get; set; }
+    [Required]
+    [StringLength(50)]
+    public required string BranchName { get; set; }
+    public int OrganizationId { get; set; }
+}
