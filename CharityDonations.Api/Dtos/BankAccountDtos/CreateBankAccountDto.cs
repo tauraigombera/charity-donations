eb284504e38b62d48312de22144ea76164b818fe
TauG <tauraigombera@gmail.com>
2023-08-03 12:40:43 +0200

    added BankAccount Dtos

---
+++ CharityDonations.Api/Dtos/BankAccountDtos/CreateBankAccountDto.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CharityDonations.Api.BankAccountDtos;
+
+public record CreateBankAccountDto
+(
+    [Required] [StringLength(20)] string BankAccount,
+    [Required] [StringLength(50)] string AccountHolderName,
+    [Required] [StringLength(50)] string BankName,
+    [Required] [StringLength(50)] string BranchName
+);
