4a1f4aeefefd6c18a0bc2984aac1b883f2168d76
TauG <tauraigombera@gmail.com>
2023-08-14 17:53:52 +0200

    added an interface IAuthenticationRepository

---
+++ CharityDonations.Api/CoreRepositories/IAuthenticationRepository.cs
@@ -0,0 +1,9 @@
+﻿using CharityDonations.Api.Dtos.RequestDtos;
+
+namespace CharityDonations.Api.CoreRepositories;
+
+public interface IAuthenticationRepository
+{
+    Task<string> Register(RegisterRequestDto request);
+    Task<string> Login(LoginRequestDto request);
+}
