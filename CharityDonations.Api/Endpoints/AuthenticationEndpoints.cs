7b80858ae803dd5d43ad8c9105699bc345fe8d1f
TauG <tauraigombera@gmail.com>
2023-08-16 07:54:39 +0200

    added authentication logic

---
+++ CharityDonations.Api/Endpoints/AuthenticationEndpoints.cs
@@ -0,0 +1,28 @@
+﻿using CharityDonations.Api.CoreRepositories;
+using CharityDonations.Api.Dtos.RequestDtos;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CharityDonations.Api.Endpoints;
+
+public static class AuthenticationEndpoints
+{
+    public static RouteGroupBuilder MapAuthenticationEndpoints(this IEndpointRouteBuilder routes)
+    {
+        var group = routes.MapGroup("/User")
+                .WithParameterValidation();
+
+        group.MapPost("/login", async ([FromBody] LoginRequestDto request, IAuthenticationRepository authService) =>
+        {
+            var response = await authService.Login(request);
+            return Results.Ok(response);
+        });
+
+        group.MapPost("/register", async ([FromBody] RegisterRequestDto request, IAuthenticationRepository authService) =>
+        {
+            var response = await authService.Register(request);
+            return Results.Ok(response);
+        });
+
+        return group;
+    } 
+}
