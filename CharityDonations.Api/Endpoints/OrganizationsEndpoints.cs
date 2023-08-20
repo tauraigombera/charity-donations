4df245d9d165844527e571c6ea53c992e5f17528
TauG <tauraigombera@gmail.com>
2023-08-20 20:53:50 +0200

    Changed implementation of OrganizationEndpoints

--- CharityDonations.Api/Endpoints/OrganizationsEndpoints.cs
+++ CharityDonations.Api/Endpoints/OrganizationsEndpoints.cs
@@ -1,6 +1,7 @@
 ﻿using CharityDonations.Api.Models;
 using CharityDonations.Api.CoreRepositories;
 using CharityDonations.Api.Dtos.OrganizationDtos;
+using Microsoft.AspNetCore.Http.HttpResults;
 
 namespace CharityDonations.Api.Endpoints;
 public static class OrganizationsEndpoints
@@ -8,64 +9,68 @@ public static class OrganizationsEndpoints
     const string GetOrganizationEndpointName = "GetOrganization";
     public static RouteGroupBuilder MapOrganizationsEndpoints(this IEndpointRouteBuilder routes)
     {
-        var group = routes.MapGroup("/organizations")
-                .WithParameterValidation();
-
-        //Get organizations
-        group.MapGet("/", async (IOrganizationsRepository repository) =>
-        {
-            var organizations = await repository.GetAllAsync();
-            var organizationDtos = organizations.Select(organization => organization.AsDto());
-            return Results.Ok(organizationDtos);
-        }).RequireAuthorization("organizations:read-write");
-
-        //Get organization by id
-        group.MapGet("/{id}", async (IOrganizationsRepository repository, int id) => 
-        {
-            Organization? organization = await repository.GetAsync(id);
-            return organization is not null ? Results.Ok(organization.AsDto()) : Results.NotFound();
-        })
-        .WithName(GetOrganizationEndpointName).RequireAuthorization("organizations:read-write");
-
-        //Post an organization
-        group.MapPost("/", async (IOrganizationsRepository repository, CreateOrganizationDto organizationDto) =>
-        {
-            Organization organization = new()
-            {
-                Name = organizationDto.Name,
-                Mission = organizationDto.Mission,
-                Description = organizationDto.Description,
-                ImageUrl = organizationDto.ImageUrl,
-                Contact = new Contact
-                {
-                    Email = organizationDto.Contact.Email,
-                    PhoneNumber = organizationDto.Contact.PhoneNumber,
-                    Address1 = organizationDto.Contact.Address1,
-                    Address2 = organizationDto.Contact.Address2,
-                    Address3 = organizationDto.Contact.Address3
-                },
-                BankAccount = new BankAccount
-                {
-                    AccountNumber = organizationDto.BankAccount.AccountNumber,
-                    AccountHolderName = organizationDto.BankAccount.AccountHolderName,
-                    BankName = organizationDto.BankAccount.BankName,
-                    BranchName = organizationDto.BankAccount.BranchName,
-                }
-            };
-
-            await repository.CreateAsync(organization);
-            return Results.CreatedAtRoute(GetOrganizationEndpointName, new {id = organization.Id}, organization);
-        }).RequireAuthorization("organizations:read-write");
-
-        //Update an organization
-        group.MapPut("/{id}", async (IOrganizationsRepository repository, int id, UpdateOrganizationDto updatedOrganizationDto) =>
-        {
-            Organization? existingOrganization = await repository.GetAsync(id);
-
-            if (existingOrganization is null){
-                return Results.NotFound();
-            }
+        var group = routes.MapGroup("/organizations").WithParameterValidation();
         
+        group.MapGet("/", GetAllOrganizations);
+        group.MapGet("/{id}", GetOrganizationByIdAsync).WithName(GetOrganizationEndpointName);
+        group.MapPost("/", CreateOrganizationAsync).RequireAuthorization("organizations:read-write");
+        group.MapPut("/{id}", UpdateOrganizationAsync).RequireAuthorization("organizations:read-write");
+        group.MapDelete("/{id}", DeleteOrganizationAsync).RequireAuthorization("organizations:read-write");
+
+        return group;
+    }
+
+    //Get all organizations
+    public static async Task<Ok<IEnumerable<OrganizationDto>>> GetAllOrganizations(IOrganizationsRepository repository)
+    {
+        var organizations = await repository.GetAllAsync();
+        var organizationDtos = organizations.Select(organization => organization.AsDto());
+        return TypedResults.Ok(organizationDtos);
+    }
+
+    //Get organization by id
+    public static async Task<Results<Ok<OrganizationDto>, NotFound>> GetOrganizationByIdAsync(IOrganizationsRepository repository, int id)
+    {
+        Organization? organization = await repository.GetAsync(id);
+        return organization is not null ? TypedResults.Ok(organization.AsDto()) : TypedResults.NotFound();
+    }
+
+    //Create organization
+    public static async Task<Created<Organization>> CreateOrganizationAsync (IOrganizationsRepository repository, CreateOrganizationDto organizationDto)
+    {
+        Organization organization = new()
+        {
+            Name = organizationDto.Name,
+            Mission = organizationDto.Mission,
+            Description = organizationDto.Description,
+            ImageUrl = organizationDto.ImageUrl,
+            Contact = new Contact
+            {
+                Email = organizationDto.Contact.Email,
+                PhoneNumber = organizationDto.Contact.PhoneNumber,
+                Address1 = organizationDto.Contact.Address1,
+                Address2 = organizationDto.Contact.Address2,
+                Address3 = organizationDto.Contact.Address3
+            },
+            BankAccount = new BankAccount
+            {
+                AccountNumber = organizationDto.BankAccount.AccountNumber,
+                AccountHolderName = organizationDto.BankAccount.AccountHolderName,
+                BankName = organizationDto.BankAccount.BankName,
+                BranchName = organizationDto.BankAccount.BranchName,
+            }
+        };
+
+        await repository.CreateAsync(organization);
+        return TypedResults.Created($"/organizations/{organization.Id}", organization);
+    }
+
+    //Update organization
+    public static async Task<Results<NoContent, NotFound>> UpdateOrganizationAsync (IOrganizationsRepository repository, int id, UpdateOrganizationDto updatedOrganizationDto)
+    {
+        Organization? existingOrganization = await repository.GetAsync(id);
+        
+        if (existingOrganization != null){
             existingOrganization.Name = updatedOrganizationDto.Name;
             existingOrganization.Mission = updatedOrganizationDto.Mission;
             existingOrganization.Description = updatedOrganizationDto.Description;
@@ -80,22 +85,21 @@ public static class OrganizationsEndpoints
 
             await repository.UpdateAsync(existingOrganization);
 
-            return Results.NoContent();
-        }).RequireAuthorization("organizations:read-write");
+            return TypedResults.NoContent();
+        }
+        return TypedResults.NotFound();      
+    }
 
-        //Delete an organization
-        group.MapDelete("/{id}", async (IOrganizationsRepository repository, int id) =>
+    //Delete organization
+    public static async Task<Results<NoContent, NotFound>> DeleteOrganizationAsync(IOrganizationsRepository repository, int id)
+    {
+        Organization? organization = await repository.GetAsync(id);
+
+        if (organization is not null)
         {
-            Organization? organization = await repository.GetAsync(id);
+            await repository.DeleteAsync(id);
+        } 
 
-            if (organization is not null)
-            {
-                await repository.DeleteAsync(id);
-            } 
-
-            return Results.NoContent();
-        }).RequireAuthorization("organizations:read-write");
-
-        return group;
+        return TypedResults.NoContent();
     }
 }
