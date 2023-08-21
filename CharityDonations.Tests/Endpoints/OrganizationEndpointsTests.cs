a3c32dc28aa222e79b94c74c97ddc7d5c2c9a969
TauG <tauraigombera@gmail.com>
2023-08-21 07:17:20 +0200

    implemented Delete_Organization_Returns_NoContent_When_Deleted test

--- CharityDonations.Tests/Endpoints/OrganizationEndpointsTests.cs
+++ CharityDonations.Tests/Endpoints/OrganizationEndpointsTests.cs
@@ -278,13 +278,56 @@ public class OrganizationEndpointsTests
         Assert.True(existingOrganization.Mission == updatedOrganization.Mission);
     }
 
-//     [Fact]
-//     public async Task Put_Organization_Returns_NotFound_When_Not_Found()
-//     {
-//         // Arrange
+    [Fact]
+    public async Task Delete_Organization_Returns_NoContent_When_Deleted()
+    {
+        // Arrange
+        Organization existingOrganization = new()
+        {
+            Id = 1,
+            Name = "Test Org",
+            Mission = "Test org mission",
+            Description = "Test org description",
+            ImageUrl = "https://placehold.co/100",
+            Contact = new Contact
+            {
+                Id = 1,
+                Email = "info@test.org",
+                PhoneNumber = "0888701110",
+                Address1 = "Test org address1",
+                Address2 = "Test org address2",
+                Address3 = "Test org address3"
+            },
+            BankAccount = new BankAccount
+            {
+                Id = 1,
+                AccountNumber = "1001577049",
+                AccountHolderName = "Test Org",
+                BankName = "National Bank of Malawi",
+                BranchName = "City Centre",
+            }
+        };
 
-//         // Act
+        var organizations = new List<Organization> { existingOrganization };
 
-//         // Assert  
-//     }
- }
+        var mock = new Mock<IOrganizationsRepository>();
+
+        mock.Setup(repo => repo.GetAsync(It.Is<int>(id => id == existingOrganization.Id)))
+            .ReturnsAsync(existingOrganization);
+
+        mock.Setup(repo => repo.DeleteAsync(It.Is<int>(id => id == 1)))
+            .Callback<int>(id => organizations.RemoveAt(0)) //removes the first organization
+            .Returns(Task.CompletedTask);
+
+        // Act
+        var result = await OrganizationsEndpoints.DeleteOrganizationAsync(mock.Object, existingOrganization.Id);
+
+        // Assert  
+        Assert.IsType<Results<NoContent, NotFound>>(result);
+        
+        var noContentResult = (NoContent) result.Result;
+
+        Assert.NotNull(noContentResult);
+        Assert.Empty(organizations);
+    }
+}
