7e173f82d84a308af584d653de96ec626309c0dd
TauG <tauraigombera@gmail.com>
2023-08-21 16:45:38 +0200

    added IRegisterRepository

---
+++ CharityDonations.Api/Repositories/IRegisterRepository.cs
@@ -0,0 +1,8 @@
+﻿using CharityDonations.Api.Dtos.RequestDtos;
+
+namespace CharityDonations.Api.Repositories;
+
+public interface IRegisterRepository
+{
+    Task<string> Register(RegisterRequestDto request);
+}
