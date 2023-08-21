58ea3da13efb101273a823dfba9778b7521f5022
TauG <tauraigombera@gmail.com>
2023-08-21 16:51:48 +0200

    added RegisterRepository

---
+++ CharityDonations.Api/Repositories/AuthRepositories/RegisterRepository.cs
@@ -0,0 +1,46 @@
+﻿using CharityDonations.Api.Dtos.RequestDtos;
+using CharityDonations.Api.Models;
+using Microsoft.AspNetCore.Identity;
+
+namespace CharityDonations.Api.Repositories.AuthRepositories;
+
+public class RegisterRepository : IRegisterRepository
+{
+    private readonly IConfiguration _config;
+    private readonly UserManager<User> _userManager;
+    public RegisterRepository (UserManager<User> userManager, IConfiguration config)
+    {
+        _userManager = userManager;
+        _config = config;
+    }
+    public async Task<string> Register(RegisterRequestDto request)
+    {
+        var userByEmail = await _userManager.FindByEmailAsync(request.Email);
+        var userByUsername = await _userManager.FindByNameAsync(request.Username);
+        if (userByEmail is not null || userByUsername is not null)
+        {
+            throw new ArgumentException($"User with email {request.Email} or username {request.Username} already exists.");
+        }
+
+        User user = new()
+        {
+            Email = request.Email,
+            UserName = request.Username,
+            SecurityStamp = Guid.NewGuid().ToString()
+        };
+
+        var result = await _userManager.CreateAsync(user, request.Password);
+
+        if (!result.Succeeded)
+        {
+            throw new ArgumentException($"Unable to register user {request.Username} errors: {GetErrorsText(result.Errors)}");
+        }
+        
+        return await Login(new LoginRequestDto(request.Email, request.Password));
+    }
+
+    private static string GetErrorsText(IEnumerable<IdentityError> errors)
+    {
+        return string.Join(", ", errors.Select(error => error.Description).ToArray());
+    }
+}
