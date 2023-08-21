9e71db6d4cade2e750265682aa4b38b1ed333d8b
TauG <tauraigombera@gmail.com>
2023-08-21 17:38:00 +0200

    modified LoginRepository

--- CharityDonations.Api/Repositories/AuthRepositories/LoginRepository.cs
+++ CharityDonations.Api/Repositories/AuthRepositories/LoginRepository.cs
@@ -1,5 +1,6 @@
 ﻿using CharityDonations.Api.Dtos.RequestDtos;
 using CharityDonations.Api.Models;
+using CharityDonations.Api.Services;
 using Microsoft.AspNetCore.Identity;
 
 namespace CharityDonations.Api.Repositories.AuthRepositories;
@@ -7,11 +8,13 @@ namespace CharityDonations.Api.Repositories.AuthRepositories;
 public class LoginRepository : ILoginRepository
 {
     private readonly UserManager<User> _userManager;
-    public LoginRepository (UserManager<User> userManager, IConfiguration config)
+    private readonly IAuthTokenService _authTokenService;
+    public LoginRepository (UserManager<User> userManager, IConfiguration config, IAuthTokenService authTokenService)
     {
         _userManager = userManager;
+        _authTokenService = authTokenService;
     }
-     public async Task<string> Login(LoginRequestDto request)
+    public async Task<string> Login(LoginRequestDto request)
     {
         var user = await _userManager.FindByNameAsync(request.Username) 
                    ?? await _userManager.FindByEmailAsync(request.Username);
@@ -20,8 +23,8 @@ public class LoginRepository : ILoginRepository
         {
             throw new ArgumentException($"Unable to authenticate user {request.Username}");
         }
-        // var jwtAccessToken = GetToken();
-        var jwtAccessToken = "12345";
+
+        var jwtAccessToken = _authTokenService.GetToken();
 
         return jwtAccessToken;
     }
