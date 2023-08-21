64512f39c92e549d2c42a7b216d9ffda69b50622
TauG <tauraigombera@gmail.com>
2023-08-21 18:21:39 +0200

    deleted old implementation of Auth

--- CharityDonations.Api/Repositories/IAuthRepository.cs
+++
@@ -1,9 +0,0 @@
-﻿// using CharityDonations.Api.Dtos.RequestDtos;
-
-// namespace CharityDonations.Api.Repositories;
-
-// public interface IAuthRepository
-// {
-//     Task<string> Register(RegisterRequestDto request);
-//     Task<string> Login(LoginRequestDto request);
-// }
