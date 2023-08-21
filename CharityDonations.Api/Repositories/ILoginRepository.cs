9af25588c6ae7bd2a3861bba9d06d1a1248ff196
TauG <tauraigombera@gmail.com>
2023-08-21 16:53:32 +0200

    added ILoginRepository

---
+++ CharityDonations.Api/Repositories/ILoginRepository.cs
@@ -0,0 +1,8 @@
+﻿using CharityDonations.Api.Dtos.RequestDtos;
+
+namespace CharityDonations.Api.Repositories;
+
+public interface ILoginRepository
+{
+     Task<string> Login(LoginRequestDto request);
+}
