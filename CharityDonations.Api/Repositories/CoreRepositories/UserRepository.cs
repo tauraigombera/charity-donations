e1e6878fac4002506b092a438da76b13df646cd7
TauG <tauraigombera@gmail.com>
2023-08-22 16:00:37 +0200

    added UserRepository

---
+++ CharityDonations.Api/Repositories/CoreRepositories/UserRepository.cs
@@ -0,0 +1,17 @@
+﻿using CharityDonations.Api.Dtos.RequestDtos;
+using CharityDonations.Api.Models;
+
+namespace CharityDonations.Api.Repositories.CoreRepositories;
+
+public class UserRepository : IUserRepository
+{
+    public User CreateAsync(RegisterRequestDto registerRequest)
+    {
+        return new User
+        {
+            Email = registerRequest.Email,
+            UserName = registerRequest.Username,
+            SecurityStamp = Guid.NewGuid().ToString()
+        };
+    }
+}
