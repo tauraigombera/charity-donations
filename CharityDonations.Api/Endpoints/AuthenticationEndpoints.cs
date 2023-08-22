c1234f2e5046573fbd4751a50c94f30519ecc574
TauG <tauraigombera@gmail.com>
2023-08-22 17:25:42 +0200

    changed implementation of AuthenticationEndpoints

--- CharityDonations.Api/Endpoints/AuthenticationEndpoints.cs
+++ CharityDonations.Api/Endpoints/AuthenticationEndpoints.cs
@@ -1,28 +1,36 @@
 ﻿using CharityDonations.Api.Repositories;
 using CharityDonations.Api.Dtos.RequestDtos;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Http.HttpResults;
 
 namespace CharityDonations.Api.Endpoints;
 
 public static class AuthenticationEndpoints
 {
+    // routes
     public static RouteGroupBuilder MapAuthenticationEndpoints(this IEndpointRouteBuilder routes)
     {
         var group = routes.MapGroup("/User")
-                .WithParameterValidation();
+                          .WithParameterValidation();
 
-        group.MapPost("/login", async ([FromBody] LoginRequestDto request, ILoginRepository authService) =>
-        {
-            var response = await authService.Login(request);
-            return Results.Ok(response);
-        });
+        group.MapPost("/login", LoginAsync);
 
-        group.MapPost("/register", async ([FromBody] UserRequestDto userRequest, IRegisterRepository authService) =>
-        {
-            var response = await authService.Register(userRequest);
-            return Results.Ok(response);
-        });
+        group.MapPost("/register", RegisterAsync);
 
         return group;
+    }
+
+    // login
+    public static async Task<Ok<string>> LoginAsync ([FromBody] LoginRequestDto request, ILoginRepository authService)
+    {
+        var response = await authService.Login(request);
+        return TypedResults.Ok(response);
     } 
+
+    // register
+    public static async Task<Ok<string>> RegisterAsync ([FromBody] UserRequestDto userRequest, IRegisterRepository authService)
+    {
+        var response = await authService.Register(userRequest);
+        return TypedResults.Ok(response);
+    }
 }
