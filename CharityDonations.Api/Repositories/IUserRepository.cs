fed59cf7200f27ad4eb0bf78bbecf3fed87b305a
TauG <tauraigombera@gmail.com>
2023-08-22 15:22:58 +0200

    added IUserRepository interface

---
+++ CharityDonations.Api/Repositories/IUserRepository.cs
@@ -0,0 +1,8 @@
+﻿using CharityDonations.Api.Dtos.RequestDtos;
+
+namespace CharityDonations.Api.Repositories;
+
+public interface IUserRepository
+{
+    Task CreateAsync(RegisterRequestDto registerRequest);
+}
