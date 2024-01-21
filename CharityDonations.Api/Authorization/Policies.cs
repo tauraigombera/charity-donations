6f734e8adbbeb69def95660bdeeb98c86ddaa4c2
TauG <tauraigombera@gmail.com>
2024-01-21 18:29:09 +0200

    added a policies class

---
+++ CharityDonations.Api/Authorization/Policies.cs
@@ -0,0 +1,8 @@
+﻿namespace CharityDonations.Api.Authorization;
+
+public static class Policies
+{
+    public const string ReadAccess = "read_access";
+
+    public const string WriteAccess = "write_access";
+}
