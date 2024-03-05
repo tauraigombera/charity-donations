6e135e4968ca2968f68524457aa0f1d5125dda4b
TauG <tauraigombera@gmail.com>
2024-03-05 06:48:16 +0200

    added a Model Donation.cs

---
+++ API/CharityDonations.Api/Models/Donation.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CharityDonations.Api.Models;
+
+public class Donation
+{
+    public int Id {get; set;}
+    [Required]
+    public required decimal Amount {get; set;}
+    [Required]
+    public required DateTime Date {get; set;}
+    [Required]
+    public required string TransactionStatus {get; set;}
+    public required Organization Organization { get; set; }
+
+    //public required User Donor { get; set; }
+}
