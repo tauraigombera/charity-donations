83e608ae3665ca694a7b03398c2e74d683e7ee76
TauG <tauraigombera@gmail.com>
2024-03-08 21:52:54 +0200

    added interface for donation

--- API/CharityDonations.Api/CoreRepositories/IDonationRepository.cs
+++ API/CharityDonations.Api/CoreRepositories/IDonationRepository.cs
@@ -5,4 +5,9 @@ namespace CharityDonations.Api.CoreRepositories;
 public interface IDonationRepository
 {
     Task CreateAsync(Donation donation);
+    Task<Donation?> GetAsync(int id);
+    Task<IEnumerable<Donation>> GetAllAsync();
+    Task<IEnumerable<Donation>> GetAllByOrganizationAsync(int organizationId);
+    
+    //  Task<IEnumerable<Donation>> GetAllByUserAsync(User user);
 }
