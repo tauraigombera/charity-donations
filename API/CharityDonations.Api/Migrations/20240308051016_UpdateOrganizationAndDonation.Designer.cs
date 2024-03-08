b31fa45ebdc87e958e28f8c1a717a2537a441845
TauG <tauraigombera@gmail.com>
2024-03-08 07:12:10 +0200

    added db migrations

--- API/CharityDonations.Api/Migrations/20240308051016_UpdateOrganizationAndDonation.Designer.cs
+++ API/CharityDonations.Api/Migrations/20240308051016_UpdateOrganizationAndDonation.Designer.cs
@@ -4,6 +4,7 @@ using CharityDonations.Api.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace CharityDonations.Api.Migrations
 {
     [DbContext(typeof(ApiDbContext))]
-    partial class CharityOrganizationsContextModelSnapshot : ModelSnapshot
+    [Migration("20240308051016_UpdateOrganizationAndDonation")]
+    partial class UpdateOrganizationAndDonation
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
