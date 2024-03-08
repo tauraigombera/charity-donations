74784644b87a7f50a3a9226aeaee6add5df43d1a
TauG <tauraigombera@gmail.com>
2024-03-08 08:26:35 +0200

    added CreateDonationDto

---
+++ API/CharityDonations.Api/Dtos/DonationDtos/CreateDonationDto.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+using CharityDonations.Api.Data;
+
+namespace CharityDonations.Api.Dtos.DonationDtos;
+
+public record CreateOrganizationDto(
+    [Required] decimal Amount,
+    [Required] DateTime Date,
+    [Required] [StringLength(50)] string DonorName,
+    [Required] TransactionStatus TransactionStatus,
+    [Required] CreateOrganizationDto BankAccount
+);
