33155a45315f99d2124aae0c6ce78b5e67c18ada
TauG <tauraigombera@gmail.com>
2024-03-09 07:41:04 +0200

    reorganised methods

--- API/CharityDonations.Api/CoreRepositories/IDonationRepository.cs
+++ API/CharityDonations.Api/CoreRepositories/IDonationRepository.cs
@@ -4,10 +4,11 @@ namespace CharityDonations.Api.CoreRepositories;
 
 public interface IDonationRepository
 {
-    Task CreateAsync(Donation donation);
+    
     Task<Donation?> GetAsync(int id);
     Task<IEnumerable<Donation>> GetAllAsync();
     Task<IEnumerable<Donation>> GetAllByOrganizationAsync(int organizationId);
-    
+
     //  Task<IEnumerable<Donation>> GetAllByUserAsync(User user);
+    Task CreateAsync(Donation donation);
 }
