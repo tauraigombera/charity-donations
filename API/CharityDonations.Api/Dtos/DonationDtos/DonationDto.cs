235c18eb1ff9105ffc55f7f5e44e9e42b795ca2c
TauG <tauraigombera@gmail.com>
2024-03-09 17:44:13 +0200

    added DonationDto

---
+++ API/CharityDonations.Api/Dtos/DonationDtos/DonationDto.cs
@@ -0,0 +1,13 @@
+﻿using CharityDonations.Api.Data;
+using CharityDonations.Api.Dtos.OrganizationDtos;
+
+namespace CharityDonations.Api.Dtos.DonationDtos;
+
+public record DonationDto
+(
+    int Id,
+    decimal Amount,
+    TransactionStatus TransactionStatus,
+    OrganizationDto Organization
+);
+  
