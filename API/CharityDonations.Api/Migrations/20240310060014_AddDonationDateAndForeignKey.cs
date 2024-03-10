9e6287239709f638d3cba2f4dc013046decdf78f
TauG <tauraigombera@gmail.com>
2024-03-10 19:06:02 +0200

    updated migrations

---
+++ API/CharityDonations.Api/Migrations/20240310060014_AddDonationDateAndForeignKey.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CharityDonations.Api.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddDonationDateAndForeignKey : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Date",
+                table: "Donations",
+                newName: "DonationDate");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "DonationDate",
+                table: "Donations",
+                newName: "Date");
+        }
+    }
+}
