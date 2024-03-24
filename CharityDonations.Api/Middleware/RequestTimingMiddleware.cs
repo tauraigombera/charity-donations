920ae6966b040d103ad714e51a4dfdd3c83446ae
TauG <tauraigombera@gmail.com>
2024-03-24 20:07:05 +0200

    added RequestTimingMiddleware

---
+++ CharityDonations.Api/Middleware/RequestTimingMiddleware.cs
@@ -0,0 +1,37 @@
+﻿using System.Diagnostics;
+
+namespace CharityDonations.Api.Middleware;
+
+public class RequestTimingMiddleware
+{
+    private readonly RequestDelegate next;
+    private readonly ILogger<RequestTimingMiddleware> logger;
+    public RequestTimingMiddleware(RequestDelegate next, ILogger<RequestTimingMiddleware> logger)
+    {
+        this.next = next;
+        this.logger = logger;
+    }
+
+    public async Task InvokeAsync(HttpContext context)
+    {
+        var stopWatch = new Stopwatch();
+
+        try
+        {
+            stopWatch.Start();
+            await next(context);
+        }
+        finally
+        {
+            stopWatch.Stop();
+
+            var elapsedMilliseconds = stopWatch.ElapsedMilliseconds;
+
+            logger.LogInformation(
+                "{RequestMethod} {RequestPath} request took {ElapsedMilliseconds}ms to complete",
+                context.Request.Method,
+                context.Request.Path,
+                elapsedMilliseconds);
+        }
+    }
+}
