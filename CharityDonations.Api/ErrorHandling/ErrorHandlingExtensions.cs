896dfb333b4c232033becdb80c90d28638596437
TauG <tauraigombera@gmail.com>
2024-03-24 21:06:01 +0200

    added error handling middleware

---
+++ CharityDonations.Api/ErrorHandling/ErrorHandlingExtensions.cs
@@ -0,0 +1,42 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CharityDonations.Api.ErrorHandling;
+
+public static class ErrorHandlingExtensions
+{
+    public static void ConfigureExceptionHandler(this IApplicationBuilder app)
+    {
+        app.Run(async context =>
+        {
+            var logger = context.RequestServices.GetRequiredService<ILoggerFactory>()
+                                .CreateLogger("Error Handling");
+
+            var exceptionDetails = context.Features.Get<IExceptionHandlerFeature>();
+            var exception = exceptionDetails?.Error;
+
+            logger.LogError(exception, "Could not process a request on machine {Machine}. TraceID: {TraceId}",
+            Environment.MachineName, Activity.Current?.TraceId);
+
+            var problem = new ProblemDetails
+            {
+                Title = "An error occurred while processing the request.",
+                Status = StatusCodes.Status500InternalServerError,
+                Extensions =
+                {
+                    {"traceId", Activity.Current?.TraceId.ToString()}
+                }
+            };
+
+            var environment = context.RequestServices.GetRequiredService<IHostEnvironment>();
+
+            if (environment.IsDevelopment())
+            {
+                problem.Detail = exception?.ToString();
+            }
+
+            await Results.Problem(problem).ExecuteAsync(context);
+        });
+    }
+}
